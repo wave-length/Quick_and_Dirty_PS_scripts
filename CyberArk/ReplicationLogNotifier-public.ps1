e2d100a3a9d06fed5664e199c381433fb2d02ecd
wavelength <46011341+wave-length@users.noreply.github.com>
2022-06-20 10:21:20 -0400

    Add files via upload
    Initial import

---
+++ CyberArk/ReplicationLogNotifier-public.ps1
@@ -0,0 +1,47 @@
+﻿#
+# CyberArk Replication Status reporter (public version)
+# This quick and dirty script can be run on your CyberARK DR vault to check the logs for failures.
+#
+
+$RunTime = Get-Date
+$PADRLogString = ""
+$PADRLogEvents = Get-Content -Tail 25 -Path "E:\Program Files (x86)\PrivateArk\PADR\Logs\padr.log"
+$EmailFromAddress = ""
+$EmailToAddress = ""
+$EmailCCAddress = ""
+$EmailBCCAddress = ""
+$SMTPServer = ""
+
+#
+# This for loop convers the logs in array format to a continuous string so that they can be added to the body of the notification email...
+#
+foreach ($line in $PADRLogEvents) {
+   $PADRLogString = $PADRLogString + "`n" + $line
+}
+
+foreach ($event in $PADRLogEvents) {
+  if (($event.Contains("PADR0013E")) -Or (($event.Contains("PADR0022I")))){ 
+    $ReplicationSuccessful = "FALSE"
+    #echo "CyberArk Replication has failed`n`n====  Failure Event ====`n$event`n`nSee remediation procedure at: http://xyleminc.atlassian.com/wiki/spaces/MSO/pages/5507942273/CyberArk+Replication+Verification+Procedure"
+    Send-MailMessage -From $EmailFromAddress -To $EmailToAddress -Cc $EmailCCAddress -Bcc $EmailBCCAddress -Subject "Cyber-Ark Replication Failed"  -Priority High -Body "CyberArk Replication has failed`n`n====  Failure Event ====`n$event`n`n==== Replication Log ====$PADRLogString" -SmtpServer $SMTPServer -Port 25
+    break
+  }
+  #
+  # Elseif required because supposedly you can't chain 'or' statements
+  #
+  elseif (($event.Contains("PADR0011E"))){ 
+    $ReplicationSuccessful = "FALSE"
+    #echo "CyberArk Replication has failed`n`n====  Failure Event ====`n$event`n`nSee remediation procedure at: http://xyleminc.atlassian.com/wiki/spaces/MSO/pages/5507942273/CyberArk+Replication+Verification+Procedure"
+    Send-MailMessage -From $EmailFromAddress -To $EmailToAddress -Cc $EmailCCAddress -Bcc $EmailBCCAddress -Subject "SaaS Cyber-Ark Replication Failed"  -Priority High -Body "CyberArk Replication has failed`n`n====  Failure Event ====`n$event`n`n==== Replication Log ====$PADRLogString" -SmtpServer $SMTPServer -Port 25
+    break
+  }
+
+  else
+    { $ReplicationSuccessful = "TRUE" }
+}
+
+if ($ReplicationSuccessful -eq "TRUE") { 
+  #echo "`nCyberArk Replication successful at last check, $RunTime " 
+   Send-MailMessage -From $EmailFromAddress -To $EmailToAddress -Subject "Cyber-Ark Replication Status"  -Priority High -Body "CyberArk Replication successful at last check, $RunTime`n`n==== Replication Log ====$PADRLogString" -SmtpServer $SMTPServer -Port 25
+}
+
